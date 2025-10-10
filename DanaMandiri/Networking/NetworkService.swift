a616af49d2a736140b63358be4d2a1bcd10a10ca
hekang1992 <hekang19921111@hotmail.com>
2025-10-10 09:12:33 +0800

    ref:测试数据请求

--- DanaMandiri/Networking/NetworkService.swift
+++ DanaMandiri/Networking/NetworkService.swift
@@ -7,24 +7,32 @@
 
 import Foundation
 import Alamofire
+import UIKit
 
 class NetworkManager {
     
     static let shared = NetworkManager()
     private init() {}
     
-    // MARK: GET 请求
+    private let session: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 30
+        configuration.timeoutIntervalForResource = 30
+        return Session(configuration: configuration)
+    }()
+    
+    // MARK: GET
     func getRequest(url: String, parameters: [String: Any]? = nil, headers: HTTPHeaders? = nil, completion: @escaping (Result<Data, AFError>) -> Void) {
-        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: headers)
+        session.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: headers)
             .validate()
             .responseData { response in
                 completion(response.result)
             }
     }
     
-    // MARK: POST JSON 请求
+    // MARK: POST JSON
     func postJsonRequest(url: String, json: [String: Any]?, headers: HTTPHeaders? = ["Content-Type": "application/json"], completion: @escaping (Result<Data, AFError>) -> Void) {
-        AF.upload(multipartFormData: { multipartFormData in
+        session.upload(multipartFormData: { multipartFormData in
             if let params = json {
                 for (key, value) in params {
                     if let str = value as? String, let data = str.data(using: .utf8) {
@@ -37,17 +45,16 @@ class NetworkManager {
         .responseData { response in
             completion(response.result)
         }
-        
     }
     
-    // MARK: 图片上传
+    // MARK: IMAGE
     func uploadImage(url: String, image: UIImage, parameters: [String: Any]? = nil, imageName: String = "file", headers: HTTPHeaders? = nil, completion: @escaping (Result<Data, AFError>) -> Void) {
         
         guard let imageData = image.jpegData(compressionQuality: 0.25) else {
             return
         }
         
-        AF.upload(multipartFormData: { multipartFormData in
+        session.upload(multipartFormData: { multipartFormData in
             multipartFormData.append(imageData, withName: imageName, fileName: "image.jpg", mimeType: "image/jpeg")
             
             if let params = parameters {
